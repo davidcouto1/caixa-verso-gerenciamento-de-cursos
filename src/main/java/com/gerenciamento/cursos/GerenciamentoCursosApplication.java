7b74715165807d89849de32150cd3d408a768c05
David <davidcouto1@gmail.com>
2026-01-27 09:26:09 -0300

    Add Classe ger. de cursos

---
+++ src/main/java/com/gerenciamento/cursos/GerenciamentoCursosApplication.java
@@ -0,0 +1,16 @@
+package com.gerenciamento.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação Spring Boot.
+ * Responsável por inicializar o sistema de gerenciamento de cursos.
+ */
+@SpringBootApplication
+public class GerenciamentoCursosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoCursosApplication.class, args);
+    }
+}
